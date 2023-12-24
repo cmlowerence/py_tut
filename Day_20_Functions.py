bfbfc2618fcfa5732944352310e77f1e3437503d
cmlowerence <cmlowerence2777@gmail.com>
2023-12-24 12:05:15 +0530

    Functions

---
+++ Day_20_Functions.py
@@ -0,0 +1,18 @@
+# Two types of functions
+# > Built-in functions
+# > User-defined functions
+
+# * built-in function comes with python complier and we can use them by just calling them. Some of them are:
+# ? min(), max(), len(), sum(), type(), range(), dict(), list(), tuple(), set(), print() and many more...
+# * User-defined functions: these functions are defined by user for custom use and functionality
+# syntax
+def function_name(param):
+  pass # not essentially this line
+  # Code and statement
+  
+# Example
+def name(fname,lname):
+  print("Hello,",fname,lname)
+
+# calling a function
+name("Tony","Stank")
