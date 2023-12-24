8d8164a82472bd0bd8f6615b7a06e7ffddfb563c
cmlowerence <cmlowerence2777@gmail.com>
2023-12-24 22:31:49 +0530

    f-strings

---
+++ Day_28-f String.py
@@ -0,0 +1,12 @@
+# String formatting in python
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49))
+
+# f-strings in python
+# Examples
+val = 'Geeks'
+print(f"{val} for {val} is a portal for {val}.")
+name = "Rakesh"
+age = 21
+print(f"Hello, My name is {name} and I'm {age} years old.")
+print(f"{2*30}")
