ad6febd0e71488cb6d7b584e9a785392c685b0fc
cmlowerence <cmlowerence2777@gmail.com>
2023-12-27 21:30:26 +0530

    Virtual Environment

---
+++ Day_43_virturalEnvironment.py
@@ -0,0 +1,4 @@
+# Virtual Environment
+# ? A virtual environment is a tool used to isolate specific python environments on a single machine, allowing you to work on multiple projects with different dependencies and packages without conflicts. This can be especially useful when working on projects that have conflicting package versions or packages that are not compatible with each other.
+
+# * To create virtual environment, we can use the venv module that comes with python.
