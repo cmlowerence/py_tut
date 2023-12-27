6ea548ba3f4d76bcf25c216b81dd757b7bc8b64c
cmlowerence <cmlowerence2777@gmail.com>
2023-12-27 21:14:34 +0530

    Shorthand if-else

---
+++ Day_41_Shorthand_if-else.py
@@ -0,0 +1,13 @@
+# if...else in one line
+# ? There is also a shorthand syntax for the if-else statement that can be used when the condition being tested is simple and the code blocks to be executed are short.
+# Example
+a = 2
+b = 320
+print("A") if a > b else print("B")
+
+# We can have multiple if-else statements on the same line
+a = 330
+b = 330
+print('A') if a > b else print('=') if a == b else print('B')
+# Another Example
+# result = value_if_true if condition else value_if_false
