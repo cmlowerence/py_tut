1f96d0832ab3cf0e8b6930f83863acba853cd8e3
cmlowerence <cmlowerence2777@gmail.com>
2023-12-31 15:19:36 +0530

    Intro to OOP

---
+++ Day_56_OOP-Intro/main.py
@@ -0,0 +1,18 @@
+# Introduction to Object -oriented programming
+# Introduction to Object-Oriented programming in Python: In programming languages, mainly there are two approaches that are used to write program or code
+# * 1) Procedural Programming
+# * 2) Object-Oriented Programming
+
+# ? The procedure we are following till now is the "Procedural Programming" approach. SO, in this session, we will learn about Object-Oriented Programming (OOP). The basic idea of OOP in Python is to use classes and objects to represent real-world concepts and entities
+
+# * A class is a blueprint or template for creating objects. It defines the properties and methods that an object of that class will have. Properties are the data or state of an object, and methods are the actions or behaviors that an object can perform.
+
+# ? An object is an instance of a class, and it contains its own data and methods. For example, we could create a class called "Person" that has properties such as name and age, and methods such as speak() and walk(). Each instance of the Person class would be a unique object with its own name and age, but they would all have the same methods to speak and walk.
+
+# * One of teh key features of OOP in python is encapsulation, which means that the internal state of an object is hidden and can only be accessed or modified through object's methods. This helps to protect the object's data and prevent it from being modified in unexpected ways.
+
+# ? Another key feature of OOP in Python is inheritance, which allows new classes to be created that inherit the properties and methods of an existing class. This allows for code reuse and maked it easy to create new classes that have similar functionality to existing classes.
+
+# * Polymorphism is also supported in Python, which means that object of different classes can be treated as if they were objects of a common class. This allows for greater flexibility in code and makes it easier to write code that can word with multiple types of objects.
+
+# ! In Summery, OOP in Python allows developers to model real-world concepts and entities using classes and objects, encapsulate data, reuse code through inheritance, and write more flexible code through polymorphism
